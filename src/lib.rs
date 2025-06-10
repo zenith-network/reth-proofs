f8d4905136ec45be178a9aa595d07e27d664f1ee
Andrzej Broński <koxu1996@gmail.com>
2025-06-10 21:33:20 +0000

    Handle invalid RPC url.

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,20 @@
 use alloy_provider::RootProvider;
 
-pub fn create_provider(http_rpc_url: &str) -> alloy_provider::RootProvider<alloy_provider::network::Ethereum> {
-  let url = http_rpc_url.try_into().unwrap();
+#[derive(Debug, thiserror::Error)]
+pub enum Error {
+  #[error("Invalid URL: {0}")]
+  InvalidUrl(String),
+}
+
+pub fn create_provider(
+  http_rpc_url: &str,
+) -> Result<alloy_provider::RootProvider<alloy_provider::network::Ethereum>, Error> {
+  let url = http_rpc_url
+    .try_into()
+    .map_err(|e| Error::InvalidUrl(format!("{}", e)))?;
   let provider = RootProvider::new_http(url);
 
-  provider
+  Ok(provider)
 }
 
 #[cfg(test)]
@@ -14,6 +24,13 @@ mod tests {
   #[test]
   fn test_create_provider() {
     let mock_url = "https://google.com";
-    create_provider(mock_url);
+    create_provider(mock_url).unwrap();
+  }
+
+  #[test]
+  fn test_create_provider_with_invalid_url() {
+    let invalid_mock_url = "foo-bar";
+    let maybe_provider = create_provider(invalid_mock_url);
+    assert!(maybe_provider.is_err());
   }
 }
