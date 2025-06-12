95f6893d339a06e11fc401a22f9d1230e2723448
Andrzej Broński <koxu1996@gmail.com>
2025-06-12 12:26:45 +0000

    Add binary for experimenting with block and witness.

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+#[tokio::main]
+async fn main() {
+  store_latest_block_and_witness().await;
+}
+
+async fn store_latest_block_and_witness() {
+  // Fetch the latest block and its witness.
+  let provider = reth_proofs::create_provider("http://130.250.187.55:8545").unwrap();
+  let block_number = reth_proofs::get_last_block_number(&provider).await.unwrap();
+  let block = reth_proofs::fetch_block(&provider, block_number)
+    .await
+    .unwrap()
+    .unwrap();
+  let witness = reth_proofs::fetch_block_witness(&provider, block_number)
+    .await
+    .unwrap();
+
+  // Store them in files.
+  reth_proofs::save_block_in_file(&block).await.unwrap();
+  reth_proofs::save_block_witness_in_file(&witness, block_number)
+    .await
+    .unwrap();
+  println!("Block {} and its witness saved successfully!", block_number);
+}
