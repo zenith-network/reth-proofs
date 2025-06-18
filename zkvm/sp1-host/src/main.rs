3e2368e5e1f9cb755dcebb458e13e5a51aaec42c
Andrzej Broński <koxu1996@gmail.com>
2025-06-18 21:49:13 +0000

    Include SP1 guest ELF in the host.

--- zkvm/sp1-host/src/main.rs
+++ zkvm/sp1-host/src/main.rs
@@ -1,3 +1,8 @@
+use sp1_sdk::include_elf;
+
+/// The ELF (executable and linkable format) file for the Succinct RISC-V zkVM.
+pub const GUEST_ELF: &[u8] = include_elf!("reth-proofs-zkvm-sp1-guest");
+
 fn main() {
   // TODO
 }
