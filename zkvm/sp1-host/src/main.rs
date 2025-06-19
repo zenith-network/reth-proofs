04fcee19c39906cf4120f0c267ffe79eb421b937
Andrzej Broński <koxu1996@gmail.com>
2025-06-19 09:37:30 +0000

    Run GPU prover in SP1 host.

--- zkvm/sp1-host/src/main.rs
+++ zkvm/sp1-host/src/main.rs
@@ -1,8 +1,20 @@
-use sp1_sdk::include_elf;
+use sp1_sdk::client::ProverClient;
+use sp1_sdk::{Prover, SP1Stdin, include_elf};
 
 /// The ELF (executable and linkable format) file for the Succinct RISC-V zkVM.
 pub const GUEST_ELF: &[u8] = include_elf!("reth-proofs-zkvm-sp1-guest");
 
 fn main() {
-  // TODO
+  let stdin = SP1Stdin::new();
+
+  println!("Creating GPU prover...");
+  let prover = ProverClient::builder().cuda().build();
+
+  println!("Generating proving bundle...");
+  let (pk, _vk) = prover.setup(GUEST_ELF);
+
+  println!("Proving execution...");
+  let _proof = prover.prove(&pk, &stdin).compressed().run().unwrap();
+
+  println!("Done!");
 }
