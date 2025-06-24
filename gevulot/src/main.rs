fe666693edc2a4f58068eed78c27c571653ae7ca
Andrzej Broński <koxu1996@gmail.com>
2025-06-24 15:29:29 +0000

    Enable proving key generation.

--- gevulot/src/main.rs
+++ gevulot/src/main.rs
@@ -1,5 +1,7 @@
 mod cli;
 
+mod sp1;
+
 #[tokio::main]
 pub async fn main() -> eyre::Result<()> {
   // Parse the command line arguments.
@@ -9,10 +11,10 @@ pub async fn main() -> eyre::Result<()> {
       let output_path = args.output_path.clone();
       // Load RSP client binary.
       let elf = sp1_sdk::include_elf!("reth-proofs-zkvm-sp1-guest").to_vec();
-      // println!("Generating PK with CPU...");
-      // let pk = sp1::generate_pk_with_cpu(elf)?;
-      // sp1::store_pk_to_file(&pk, &output_path)?;
-      // println!("PK stored to {}", output_path.display());
+      println!("Generating PK with CPU...");
+      let pk = sp1::generate_pk_with_cpu(elf)?;
+      sp1::store_pk_to_file(&pk, &output_path)?;
+      println!("PK stored to {}", output_path.display());
       return Ok(());
     }
     cli::Command::Run(args) => args,
