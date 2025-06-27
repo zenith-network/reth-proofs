4c175ae321453b87da57b764b2fbc3ad33192793
Andrzej Broński <koxu1996@gmail.com>
2025-06-27 14:20:21 +0000

    Update Risc0 host to pass zkVM input.

--- zkvm/risc0-host/src/main.rs
+++ zkvm/risc0-host/src/main.rs
@@ -1,17 +1,33 @@
 use reth_proofs_zkvm_risc0_methods::RETH_PROOFS_ZKVM_RISC0_GUEST_ELF;
 use risc0_zkvm::{ExecutorEnv, default_executor};
 
-fn main() -> anyhow::Result<()> {
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
   tracing_subscriber::fmt()
     .with_env_filter(tracing_subscriber::filter::EnvFilter::from_default_env())
     .init();
 
-  let iterations: u32 = 1000;
+  // Prepare zkVM input from offline RPC data.
+  let block_number = 22724090_u64;
+  let witness = reth_proofs::load_block_witness_from_file(block_number)
+    .await
+    .unwrap();
+  let block_rpc = reth_proofs::load_block_from_file(block_number)
+    .await
+    .unwrap();
+  let block_consensus = reth_proofs::rpc_block_to_consensus_block(block_rpc);
+  let input = reth_proofs_core::input::ZkvmInput::from_offline_rpc_data(block_consensus, &witness);
+  let input_bytes = bincode::serialize(&input).unwrap();
+  let input_bytes_len = input_bytes.len();
 
   // Execute the guest code.
-  let env = ExecutorEnv::builder().write(&iterations)?.build()?;
+  let env = ExecutorEnv::builder()
+    .write(&input_bytes_len)?
+    .write_slice(&input_bytes)
+    .build()?;
   let exec = default_executor();
   exec.execute(env, RETH_PROOFS_ZKVM_RISC0_GUEST_ELF)?;
+  println!("Done!");
 
   Ok(())
 }
